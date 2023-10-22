04df48c2e637b4e364617b86f77cac7144a96548
Azat <kushkarov.azat@gmail.com>
2023-10-22 18:51:00 +0300

    commit in lesson3

--- src/main/java/org/example/lesson3/les3.java
+++ src/main/java/org/example/lesson3/les3.java
@@ -2,6 +2,7 @@ package org.example.lesson3;
 
 public class les3 {
     public static void main(String[] args) {
-        System.out.println("lesson 3");
+        System.out.println("lesson 3!!!");
+        System.out.println("lesson 3!!!");
     }
 }
