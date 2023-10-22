9102aa6351e2c4dd02b3f8c6c93eb3aef5f8cca4
Azat <kushkarov.azat@gmail.com>
2023-10-22 17:37:22 +0300

    commit in lesson1

--- src/main/java/org/example/Lesson1.java
+++ src/main/java/org/example/Lesson1.java
@@ -4,5 +4,8 @@ import java.util.Scanner;
 
 public class Lesson1 {
     Scanner scanner = new Scanner(System.in);
-    
+
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
 }
